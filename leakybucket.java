2fe1d385cb7da0ca10a3ab07942c24d3f3501384
nayana12344567 <Nayana.23cs080@sode-edu.in>
2025-11-13 20:39:44 +0530

    Update

---
+++ leakybucket.java
@@ -0,0 +1,61 @@
+
+package Pakg;
+import java.util.Scanner;
+public class Leaky {
+
+   
+    public static void main(String[] args) {
+        int i;
+        int a[]=new int[20];
+        int buck_rem=0,sent,recv;
+        Scanner in=new Scanner(System.in);
+        System.out.println("enter the number of packet");
+        int n=in.nextInt();
+        System.out.println("enter the bucket capacity:");
+        int buck_cap=in.nextInt();
+        
+        System.out.println("enter the output rate:");
+        int rate=in.nextInt();
+        System.out.println("enter the size of packet:");
+        for(i=1;i<=n;i++)
+        a[i]=in.nextInt();
+        System.out.println("CLOCK\tPACKET SIZE\tACCEPT\tSENT\tREMAINING");
+        for(i=1;i<=n;i++)
+        {
+            if(a[i]!=0)
+            {
+                if(buck_rem+a[i]>buck_cap)
+                    recv=1;
+                else{
+                    recv=a[i];
+                    buck_rem+=a[i];
+                }
+            }
+            else
+                recv=0;
+            if(buck_rem!=0)
+            {
+                if(buck_rem<rate)
+                {
+                    sent=buck_rem;
+                    buck_rem=0;
+                }
+                else
+                {
+                    sent=rate;
+                    buck_rem=buck_rem-rate;
+                }
+            }
+            else
+                sent=0;
+            if(recv==-1)
+                System.out.println(i +"\t\t"+ a[i]+"\tdropeed\t"+sent+"\t"+buck_rem);
+            else
+                System.out.println(i+"\t\t"+a[i]+"\t\t"+recv+"\t"+sent+"\t"+buck_rem);
+        }
+    }
+}
+        
+
+
+        
