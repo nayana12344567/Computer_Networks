2950d8d23fac91d9a8a3b09219e878b7f5471042
nayana12344567 <Nayana.23cs080@sode-edu.in>
2025-11-13 20:41:59 +0530

    Update

---
+++ bellmanford.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+public class BellmanDemoFinal {
+     static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+
+        int V, E=1, chckNegative = 0;
+        int w[][]=new int[20][20];
+        int edge[][]=new int[50][2];
+
+        System.out.print("Enter the no of vertices: ");
+        V = in.nextInt();
+        System.out.print("Enter the weight matrix:\n");
+        for  (int i = 1; i <= V; i++) 
+            for (int j = 1; j <= V; j++) {
+                w[i][j] = in.nextInt();
+                if (w[i][j] != 0) {
+                    edge[E][0] = i;
+                    edge[E][1] = j;
+                }
+            }
+
+        chckNegative = bellmanFord(w, V, E, edge);
+
+        if (chckNegative == 1)
+            System.out.println("\n No negative weight cycle in graph\n");
+        else
+            System.out.println(" negative weight cycle is exist");
+    }
+
+    public static int bellmanFord(int w[][] , int V, int E, int edge[][]) {
+        int u,v,S,flag=1;
+        int distance[] = new int[20];
+        int parent []= new int[20];
+
+        for (int i = 1; i <= V; i++) {
+            distance[i] = 999;
+            parent[i] = -1;
+        }
+
+        System.out.print("Enter the source vertex: ");
+        S = in.nextInt();
+        distance[S] = 0;
+
+        // Relax edges (V-1) times
+        for (int i = 1; i <= V - 1; i++) {
+            for (int k = 0; k <= E; k++) {
+                u = edge[k][0];
+                v = edge[k][1];
+                if (distance[u] + w[u][v] < distance[v]) {
+                    distance[v] = distance[u] + w[u][v];
+                    parent[v] = u;
+                }
+            }
+        }
+
+        // Check for negative weight cycle
+        for (int k = 0; k <= E; k++) {
+            u = edge[k][0];
+            v = edge[k][1];
+            if (distance[u] + w[u][v] < distance[v]) 
+                flag = 0;
+            
+        }
+
+        if (flag == 1) 
+            for(int i=1;i<=V;i++)
+            System.out.println("vertex"+i+"->cost="+distance[i]+"parent="+(parent[i]));
+            return  flag;
+    }
+}
